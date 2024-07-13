dfb324518729b9bc89a903611079d213503c0d5e
kunjumontv <kunjumontv@gmail.com>
2024-07-13 23:14:07 +0530

    first commit

---
+++ day01/dayOneChallenge.js
@@ -0,0 +1,56 @@
+// Activity 1: Variable Declaration
+
+// Task 1
+var number = 100;
+console.log(number);
+
+// Task2
+let channel = "chai aur code";
+console.log(channel);
+
+// Activity 2: Constant Declaration
+
+// Task 3
+const succeed = true;
+console.log(succeed);
+
+// Activity 3: Data Types
+
+// Task 4
+let firstName = "Mahir";
+console.log(typeof firstName);
+
+let age = 35;
+console.log(typeof age);
+
+let married = true;
+console.log(typeof married);
+
+let address = {
+  houseNumber: "M456",
+  city: "Aluva",
+  state: "Kerala",
+};
+console.log(typeof address);
+
+let taskCompleted = ["Task1", "Task2", "Task3", "Task4"];
+console.log(typeof taskCompleted);
+
+// Activity 4: Reassigning Variables
+
+// Task 5
+let currentCity = "Kochi"
+console.log("Task 5", currentCity)
+
+currentCity = "Dubai"
+console.log("Task 5 Reassign", currentCity)
+
+// Activity 5: Understaind const
+
+// Task 6
+const valueOfPi = "3.14159265359"
+console.log("Task 6", valueOfPi)
+
+valueOfPi = "3.24444444"
+console.log("Task 6 Reassign const", valueOfPi) // Type Error
+
